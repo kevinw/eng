dd5614c5369fedea49d3bb9dc8c44134c8819756
Kevin Watters <kevinwatters@gmail.com>
2020-12-28 12:40:10 +0100

    some rudimentary editor WASD flycam

--- resources/shaders/unlit_texture.hlsl
+++ resources/shaders/unlit_texture.hlsl
@@ -1,9 +1,17 @@
+//foo.bar.
+// TODO: This should probably remain a Texture2D. the input for this im renderer shader isn't a multiview texture.
+// i'm just happening to use it to blit ONE of the views to screen. instead, let's make a special purpose "multiview blit"
+// shader for this? and then flip through them in time? and then provide multiple projection matrices to this shader instead.
+// THAT is the multiview variable that makes sense here.
+
+
+//Texture2DArray color_tex       : register(t0);
 Texture2D color_tex            : register(t0);
 SamplerState color_tex_sampler : register(s0);
 
 cbuffer constants : register(b0)
 {
-    float4x4 projection;
+    float4x4 view_projection;
 }
 
 struct vs_in
@@ -22,7 +30,7 @@ struct vs_out
 
 vs_out vs_main(vs_in input) {
     vs_out output;
-    output.position = mul(float4(input.position.x, input.position.y, 0, 1), projection);
+    output.position = mul(float4(input.position.x, input.position.y, 0, 1), view_projection);
     output.texcoord = input.texcoord;
     output.color    = input.color;
     return output;
@@ -30,4 +38,6 @@ vs_out vs_main(vs_in input) {
 
 float4 ps_main(vs_out input): SV_TARGET {
     return input.color * color_tex.Sample(color_tex_sampler, input.texcoord);
+    //int layer = 0;
+    //return input.color * color_tex.Sample(color_tex_sampler, float3(input.texcoord, layer));
 }
