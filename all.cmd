3349a9f0d9b42d04910fdf11b55dcd8ee9ad61ee
Kevin Watters <kevinwatters@gmail.com>
2021-02-08 12:40:11 +0100

    we're running again but still have a crash

--- all.cmd
+++ all.cmd
@@ -1,1 +1,5 @@
+if exist game_dev.exe (
+    del game_dev.exe || exit /b 1
+)
+
 @jai first.jai -- all && game_dev.exe %*
---
+++ clean.bat
@@ -0,0 +1,5 @@
+if exist game.exe del game.exe
+if exist game_dev.exe del game_dev.exe
+if exist null_plugin.dll del null_plugin.dll
+if exist d3d_game.dll del d3d_game.dll
+if exist shmup.dll del shmup.dll
