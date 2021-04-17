cc936b339fb0b8c78d0ebca17c695d562f6db4b7
Kevin Watters <kevinwatters@gmail.com>
2021-04-17 11:56:54 +0200

    stop ignoring dlls; make sure dependencies included in the PATH in r.cmd

--- lib/ozz/examples/copy_libs.bat
+++
@@ -1,4 +0,0 @@
-@set BUILD_DIR=C:\src\ozz-animation\build-dll\samples\framework\Release
-copy %BUILD_DIR%\sample_framework_r.dll . || exit /b 1
-copy %BUILD_DIR%\sample_framework_r.dll ..\lib\win\release || exit /b 1
-copy %BUILD_DIR%\sample_framework_r.lib ..\lib\win\release || exit /b 1
--- r.cmd
+++ r.cmd
@@ -1,3 +1,5 @@
 @setlocal
 @set PATH=lib\imgui_docking\win\dll;%PATH%
+@set PATH=lib\assimp;%PATH%
+@set PATH=lib\lz4-1.9.3\win\dll;%PATH%
 jai first.jai -- %* && game_dev.exe %*
