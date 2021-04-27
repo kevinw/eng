bcd119ebde3b8943e7c91d4cb3fcaae1b1c6de3f
Kevin Watters <kevinwatters@gmail.com>
2021-04-27 08:50:39 +0200

    -quiet

--- r.cmd
+++ r.cmd
@@ -2,4 +2,4 @@
 @set PATH=lib\imgui_docking\win\dll;%PATH%
 @set PATH=lib\assimp;%PATH%
 @set PATH=lib\lz4-1.9.3\win\dll;%PATH%
-jai first.jai -- %* && game_dev.exe %*
+jai -quiet first.jai -- %* && game_dev.exe %*
