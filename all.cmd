838be0f671a1c2437155b1189c22c1c427249012
Kevin Watters <kevinwatters@gmail.com>
2021-05-01 13:42:08 +0200

    make sure all.cmd has the correct PATH setup

--- all.cmd
+++ all.cmd
@@ -2,4 +2,7 @@ if exist game_dev.exe (
     del game_dev.exe || exit /b 1
 )
 
-@jai first.jai -- all && game_dev.exe %*
+@set PATH=lib\imgui_docking\win\dll;%PATH%
+@set PATH=lib\assimp;%PATH%
+@set PATH=lib\lz4-1.9.3\win\dll;%PATH%
+jai first.jai -- all && game_dev.exe %*
