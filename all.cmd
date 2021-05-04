c5b47fe6ae83a8b5483ecbf2730d0db8e4b8466d
Kevin Watters <kevinwatters@gmail.com>
2021-05-04 17:14:16 +0200

    we need the chipmunk dll path

--- all.cmd
+++ all.cmd
@@ -2,6 +2,7 @@ if exist game_dev.exe (
     del game_dev.exe || exit /b 1
 )
 
+@set PATH=lib\Chipmunk\win\dll;%PATH%;
 @set PATH=lib\imgui_docking\win\dll;%PATH%
 @set PATH=lib\assimp;%PATH%
 @set PATH=lib\lz4-1.9.3\win\dll;%PATH%
